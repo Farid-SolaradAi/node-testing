9ca099f78cc25f8ee714fa5dabbcc0dde8a56afb
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-09-04 11:29:46 +0530

    add /addNcmrwfData route

--- server.js
+++ server.js
@@ -121,6 +121,87 @@ app.get('/addNcmrwfMetadata', async (req, res, next) => {
 });
 
 
+app.get('/addNcmrwfData', async (req, res, next) => {
+    try {
+        //for 4.0k and 00Z
+        const resolutions = ['NCMRWF_04P0', 'NCMRWF_12P5']
+        const times = ['00Z', '12Z']
+
+        for (const resolution of resolutions) {
+            for (const time of times) {
+                let directoryPath = `/home/ubuntu/efs_nwpdata/NCMRWF/processed-data/ftp.ncmrwf.gov.in/${resolution}/${time}`;
+
+                const folders = fs.readdirSync(directoryPath)
+
+                for (const folder of folders) {
+                    let currDirPath = directoryPath + '/' + folder;
+                    const files = fs.readdirSync(currDirPath);
+
+                    for (const file of files) {
+                        let filePath = currDirPath + '/' + file
+                        const parts = file.split('_');
+                        const siteName = parts[0];
+                        const companyName = parts[1];
+
+                        //get the site id from utility_sites table
+                        const siteInDB = await pool.query("SELECT id FROM utility_sites WHERE sitename = $1 AND company = $2", [siteName, companyName]);
+
+                        //check if site exists in utility_sites table
+                        if (siteInDB.rows.length === 0) {
+                            console.log("Site does not exist in utility_sites table");
+                            res.send("Site does not exist in utility_sites table");
+                            return;
+                        }
+
+                        const siteId = siteInDB.rows[0].id;
+                        //find the time that file has been generated
+                        const stats = fs.statSync(filePath)
+                        const fileTime = moment(stats.mtime).format('YYYY-MM-DD HH:mm:ss.SSS')
+
+                        let generatedTime = fileTime
+
+                        const readableStream = fs.createReadStream(filePath)
+
+                        readableStream
+                            .pipe(csv())
+                            .on('data', async (row) => {
+                                const ncmrwf_type = resolution;
+                                const type = time;
+                                const generated_time = generatedTime;
+                                const site_id = siteId;
+                                const rowTime = row['TIME'] || null
+                                const ghi = row['SHORTWAVE_DOWNWARDS(W/m2)'] || null
+                                const precipitation = row['PRECIPITATION(mm)'] || null;
+                                const surface_pressure = row['SURF_PRES(Pa)'] || null;
+                                const sealevel_pressure = row['MEAN_SEALEV_PRES(Pa)'] || null;
+                                const humidity = row['SPHUMIDITY_2M(Kg/Kg)'] || null;
+                                const temp_2m = row['AIRTEMP_2M(degC)'] || null;
+                                const low_cloud_cover_perc = row['LOW_CLOUD_COVER(Percentage)'] || null;
+                                const wind_speed_10m = row['WINDSPEED_10M(m/s)'] || null
+                                const wind_speed_50m = row['WINDSPEED_50M(m/s)'] || null
+
+                                await pool.query("INSERT INTO ncmrwf_data (ncmrwf_type, type, generated_time, site_id, time, ghi, precipitation, surface_pressure, sealevel_pressure, humidity, temp_2m, low_cloud_cover_perc, wind_speed_10m, wind_speed_50m) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10 , $11, $12, $13, $14)",
+                                    [ncmrwf_type, type, generated_time, site_id, rowTime, ghi, precipitation, surface_pressure, sealevel_pressure, humidity, temp_2m, low_cloud_cover_perc, wind_speed_10m, wind_speed_50m]);
+
+
+                            })
+                            .on('end', () => {
+                                console.log(`CSV file successfully processed for ${site} of ${client}`);
+                            });
+                    }
+                }
+            }
+        }
+
+        res.send('Done Bro, Ab chill mar')
+    }
+    catch (err) {
+        console.log(err.message);
+        return;
+    }
+});
+
+
 const port = process.env.PORT || 3001;
 const host = process.env.HOST || `localhost`;
 
